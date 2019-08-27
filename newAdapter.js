64793599a6edb4832d317475412967f6094af3a0
Chinmay2311 <chinmay.chavan23@gmail.com>
2019-08-27 18:45:32 +0530

    Changes as on 27 Aug '19 at 6.40pm

---
+++ newAdapter.js
@@ -0,0 +1,45 @@
+let users = [];
+class ArrayAdapter{
+   save(_adapter){
+       users.push(_adapter);
+   }
+   display(){
+       console.log(users);
+   }
+   update(id,obj){
+       const user = users.find(el => id === el.id);
+       if(!user){
+           return 'User not found';
+       }
+       const index = users.indexOf(user);
+       users[index]=obj;
+   }
+}
+class User{
+   constructor(instance,schema){
+       this.instance = instance
+       this.schema =schema;
+   }
+   display(){
+       this.instance.display()
+   }
+   save(id,name){
+       this.instance.save(new this.schema(id,name));
+   }
+   update(id,obj){
+       this.instance.update(id,obj)
+   }
+}
+class Schema{
+   constructor(id,name){
+       this.id=id;
+       this.name=name;
+   }
+}
+const adapt = new ArrayAdapter();
+const user = new User(adapt,Schema);
+user.save(1,'Vasant');
+user.save(2,'Akshay');
+user.save(3,'Abhishek');
+user.update(1,new Schema(1,'Chinmay'));
+user.display()
