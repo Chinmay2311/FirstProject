5aa3fd36dd80f68bf3a9e67cdaafe951ff5aa0e8
Chinmay2311 <chinmay.chavan23@gmail.com>
2019-08-28 12:33:43 +0530

    second commit

--- newAdapter.js
+++ newAdapter.js
@@ -1,53 +1,85 @@
+
 let users = [];
-class ArrayAdapter{
-   save(_adapter){
-       users.push(_adapter);
-   }
-   display(){
-       console.log(users);
-   }
-   update(id,obj){
-        let user1 = "";
-        let ind = "";
-       users.forEach((data,index)=>{
-           if(id==data.id){
-             user1 = data;
-             ind = index
-           }
-       })
-       const user = users.find(el => id === el.id);
-       if(!user){
-           return 'User not found';
-       }
-       const index = users.indexOf(user);
-       users[index]=obj;
-   }
+class ArrayAdapter {
+    save(data) {
+        return new Promise((resolve, reject) => {
+            if (data) {
+                users.push(data);
+            }
+            else {
+                reject('data not found');
+            }
+        })
+    }
+    display() {
+        return new Promise((resolve, reject) => {
+            if (users) {
+                resolve(users);
+            }
+            else {
+                reject('data not found');
+            }
+        })
+
+    }
+    update(id, obj) {
+        return new Promise((resolve, reject) => {
+            let user1 = "";
+            let ind = "";
+            users.forEach((data, index) => {
+                if (id == data.id) {
+                    user1 = data;
+                    ind = index
+                }
+            })
+            //    const user = users.find(el => id === el.id);
+            if (!user) {
+                reject('User not found');
+            }
+            Object.keys(obj).forEach((val) => {
+                if (Object.keys(user1).includes(val)) {
+                    this.users[ind][val] = obj[val];
+                }
+            })
+            resolve(this.users[ind]);
+        })
+    }
 }
-class User{
-   constructor(instance,schema){
-       this.instance = instance
-       this.schema =schema;
-   }
-   display(){
-       return this.instance.display()
-   }
-   save(id,name){
-       this.instance.save(new this.schema(id,name));
-   }
-   update(id,obj){
-       this.instance.update(id,obj)
-   }
+class User {
+    constructor(instance, schema) {
+        this.instance = instance
+        this.schema = schema;
+    }
+    display() {
+        return this.instance.display()
+    }
+    save(id, name) {
+        return this.instance.save(new this.schema(id, name));
+    }
+    update(id, obj) {
+        return this.instance.update(id, obj)
+    }
 }
-class Schema{
-   constructor(id,name){
-       this.id=id;
-       this.name=name;
-   }
+class Schema {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
 }
-const adapt = new ArrayAdapter();
-const user = new User(adapt,Schema);
-user.save(1,'Vasant');
-user.save(2,'Akshay');
-user.save(3,'Abhishek');
-user.update(1,new Schema(1,'Chinmay'));
-user.display()
+
+const main = async () => {
+    try {
+        const adapt = new ArrayAdapter();
+        const user = new User(adapt, Schema);
+        console.log(await user.save(1, 'Vasant'));
+        console.log(await user.save(2, 'Nimesh'));
+        console.log(await user.save(3, 'Abhi'));
+        console.log(await user.update(1, 'Chinmay'));
+        console.log(await user.display());
+        user.display()
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+main();
