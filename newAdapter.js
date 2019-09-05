9bbf60740aa16a8965e9b2d338cfc83d4aebf897
Chinmay2311 <chinmay.chavan23@gmail.com>
2019-09-05 12:44:19 +0530

    Added Vehicle class

--- newAdapter.js
+++ newAdapter.js
@@ -1,22 +1,27 @@
-let MongoDB = require('./mongo');
-users = [];
+const mongoClient = require('mongodb').MongoClient;
+const dotenv = require('dotenv');
+dotenv.config({path:'./config.env'})
 class ArrayAdapter{
+    constructor(){
+        this.storage = [];
+    }
+    // storage = [];
     save(data){
         return new Promise((resolve,reject)=>{
             if(data){
-                users.push(data);
+                this.storage.push(data);
                 resolve(data);
             }else{
-                reject("Couldn't find data");
+                reject("Data is unavailable");
             }
         })
     }
     show(){
         return new Promise((resolve,reject)=>{
-            if(users){
-                resolve(users);
+            if(this.storage){
+                resolve(this.storage);
             }else{
-                reject("Couldn't find data");
+                reject("Data is unavailable");
             }
         })
     }
@@ -24,44 +29,100 @@ class ArrayAdapter{
         return new Promise((resolve,reject)=>{
             let user = "";
             let index = "";
-            users.forEach((data,ind)=>{
+            this.storage.forEach((data,ind)=>{
                 if(id==data.id){
                     user=data;
                     index=ind;
                 }
             })
             if(!user){
-                reject("User not found");
+                reject("User not available");
             }
             Object.keys(obj).forEach((val)=>{
                 if(Object.keys(user).includes(val)){
-                    users[index][val]=obj[val];
+                    this.storage[index][val]=obj[val];
                 }
             })
-            resolve(users[index]);
+            resolve(this.storage[index]);
         })
     }
  }
  class MongoAdapter{
-    async save(data){
+     constructor(){
+    this._db; 
+    this.collection;
+    this.err;
+     }
+    async connect(typeObj){
+        let flag=5;
+        /* try {
+            if(this._db){
+                return 'Connection has been intilized before'
+            }else{
+                let db = await mongoClient.connect(process.env.DATABASE);
+                this._db=db;
+                this.collection=db.db('Users').collection('user');
+                return 'Connection has started'
+            }
+        } catch (error) {
+            throw error;
+        } */
+        while(flag && !this._db){
+            await _connect(this,typeObj);
+            flag--;
+        }
+        async function _connect(refrence,typeObj){
+           try {
+               let db = await mongoClient.connect(process.env.DATABASE);
+               refrence._db=db;
+               refrence.collection=db.db(typeObj.db).collection(typeObj.collection);
+           } catch (error) {
+               refrence.err=error;
+           }
+        }
+        if(!this._db){
+            throw this.err;
+        }
+
+    }
+    async close(){
         try {
-            let user = await MongoDB.getDB().collection('user').insertOne(data);
-            //console.log(user)
-            return user.ops[0];
+            if(!this._db){
+                return 'Connection has not started so cant close the connection'
+            }else{
+                await this._db.close();
+            }
+        } catch (error) {
+            throw error;   
+        }
+    } 
+    async save(data,typeObj){
+        try {
+            if(!this._db){
+                await this.connect(typeObj);
+            }
+            let user = await this.collection.insertOne(data);
+            return user.ops[0];           
         } catch (error) {
             throw error;
         }
     }
-    async show(){
+    async show(typeObj){
         try{
-            return await MongoDB.getDB().collection('user').find().toArray()
+            if(!this._db){
+                await this.connect(typeObj);
+            }
+            return await this.collection.find().toArray() 
         } catch(error){
             throw error;
         }   
     }
-    async update(id,obj){
+    async update(id,obj,typeObj){
         try{
-            let data = await MongoDB.getDB().collection('user').findOneAndUpdate({id:id},{$set:{name:obj}});
+            if(!this._db){
+                await this.connect(typeObj);
+            }
+            let data = await this.collection.findOneAndUpdate({id:id},{$set:{name:obj}});
             return data.value;
         } catch(error){
             throw error;
@@ -69,58 +130,105 @@ class ArrayAdapter{
     }
  }
  class User{
+
+    
     constructor(instance,schema){
         this.instance = instance
         this.schema =schema;
+        this.db='Users';
+        this.collection='user'
     }
     show(){
-        return this.instance.show()
+        return this.instance.show(this)
     }
     save(id,name){
-        return this.instance.save(new this.schema(id,name));
+        return this.instance.save(new this.schema(id,name),this);
     }
     update(id,obj){
-        return this.instance.update(id,obj)
+        return this.instance.update(id,obj,this)
     }
  }
  class Schema{
     constructor(id,name){
-        // if(!id || !name){
-        //     throw new Error('id and name is required');
-        // }
+        if(!id || !name){
+            throw new Error('id and name is required');
+        }
         this.id=id;
         this.name=name;
     }
  }
- const main = async ()=>{
+ class Vehicle{
+     
+     constructor(instance,schema){
+        this.instance = instance;
+        this.schema=schema;
+        this.db='Vehicles';
+        this.collection='vehicle';
+     }
+     show(){
+         return this.instance.show(this);
+     }
+     save(id,name){
+         return this.instance.save(new this.schema(id,name),this);
+     }
+     update(id,obj){
+         return this.instance.update(id,obj,this);
+     }
+ }
+ class VehicleSchema{
+     constructor(id,name){
+         if(!id || !name){
+             throw new Error('id and name is required');
+         }
+         this.id=id;
+         this.name=name;
+     }
+ }
+ const MongoVehicleMain = async ()=>{
+    try {
+        const adapt = new MongoAdapter();
+        const vehicle = new Vehicle(adapt,VehicleSchema);
+        console.log("mongo vehicle");
+        console.log(await vehicle.save(1,'Honda'));
+        console.log(await vehicle.save(2,'Ford'));
+        console.log(await vehicle.save(3,'Bmw'));
+        console.log(await vehicle.show());
+        console.log(await vehicle.update(1,'Tata'));
+        console.log(await vehicle.show());
+        adapt.close();
+    } catch (err) {
+        console.log(err);   
+    }
+ }
+ MongoVehicleMain();
+ const MongoMain = async ()=>{
+    try {
+        const adapt = new MongoAdapter();
+        const user = new User(adapt,Schema);
+        console.log(await user.save(1,'abhishek'));
+        console.log(await user.save(2,'jaiswal'));
+        console.log(await user.save(3,'rohan'));
+        console.log(await user.show());
+        console.log(await user.update(1,'satyam'));
+        console.log(await user.show());
+        adapt.close();
+    } catch (err) {
+        console.log(err);   
+    }
+ }
+MongoMain();
+const main = async ()=>{
     try {
         const adapt = new ArrayAdapter();
         const user = new User(adapt,Schema);
-        console.log(await user.save(1,'Chinmay'));
-        console.log(await user.save(2,'Vasant'));
-        console.log(await user.save(3,'Rahul'));
+        console.log(await user.save(1,'abhishek'));
+        console.log(await user.save(2,'jaiswal'));
+        console.log(await user.save(3,'rohan'));
         console.log(await user.show());
-        console.log(await user.update(1,{name:'Rohan'}));
+        console.log(await user.update(1,{name:'satyam'}));
         console.log(await user.show());
     } catch (err) {
         console.log(err);   
     }
 }
 main();
-MongoDB.connectDB(async (err) => {
-    try{
-        if (err) throw err
-        const adapt = new MongoAdapter();
-        const user = new User(adapt,Schema);
-        console.log(await user.save(1,'Chinmay'));
-        console.log(await user.save(2,'Vasant'));
-        console.log(await user.save(3,'Rahul'));
-        console.log(await user.show());
-        console.log(await user.update(1,"Rohan"));
-        console.log(await user.show());
-        MongoDB.disconnectDB();
-    }catch (err){
-        console.log(err);
-    }
-    
-})
